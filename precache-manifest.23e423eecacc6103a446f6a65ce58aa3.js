3502c71fc29ef37580166ed6cd123dec0e85399e
Renat <vashvc123@gmail.com>
2020-05-21 23:04:26 +0300

    Updates

---
+++ precache-manifest.23e423eecacc6103a446f6a65ce58aa3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5c3acc1c211eabe311cbb515e4dee0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db5d758a36ceff6382f7",
+    "url": "/static/css/2.85b3d357.chunk.css"
+  },
+  {
+    "revision": "78720b1de2aa2e29c4c6",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "db5d758a36ceff6382f7",
+    "url": "/static/js/2.c45505bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c45505bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78720b1de2aa2e29c4c6",
+    "url": "/static/js/main.6ef5589d.chunk.js"
+  },
+  {
+    "revision": "ddac5d437404a12be9e4",
+    "url": "/static/js/runtime-main.4d4ff96d.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
